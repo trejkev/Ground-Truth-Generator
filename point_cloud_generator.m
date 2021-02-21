d1d979a8192cfffe98db3fd1c1907d9227833782
Kevin Trejos <trejkev@gmail.com>
2021-02-21 11:30:15 -0600

    Resolved angles issue

--- point_cloud_generator.m
+++ point_cloud_generator.m
@@ -33,12 +33,11 @@
 
 %%
 
-mkdir individual_models_dim_properties
-
-% Global variables
+% Model type identifiers
 actual_box = 0;
 actual_cilinder = 0;
 
+% Flags to advise if the model pose and scale are already on place
 poseFound = 0;
 scaleFound = 0;
 
@@ -49,11 +48,15 @@ yawRotation = 0;
 cylRadius = 0;
 xScale = 0;
 yScale = 0;
-figuresMatrix = zeros(1,8);
-cilindersMatrix = zeros(1,3);
 
-%header = {'Name', 'X1', 'Y1', 'X2', 'Y2', 'X3', 'Y3', 'X4', 'Y4'};
-%writecell(header, 'figures_container.xls')
+% Models matrices to keep their dimensions
+figuresMatrix = zeros(1,8);
+cylindersMatrix = zeros(1,3);
+
+% Remove last xlsx
+if exist('figures_container.xlsx', 'file') == 2
+    delete('figures_container.xlsx')
+end
 
 base_file_id = fopen('ground_truth.world', 'r');
 actual_line = string(fgetl(base_file_id));
@@ -68,6 +71,7 @@ end
 % Look for the models
 while(actual_line ~= '-1')
     
+    % Look for the model name
     actual_line_token = regexp(actual_line, 'model name', 'match');
     if (size(actual_line_token) == 1)
         model_name = get_model_name(actual_line);
@@ -75,12 +79,15 @@ while(actual_line ~= '-1')
         scaleFound = 0;
     end
     
+    % Look for the model pose
     actual_line_token = regexp(actual_line, 'pose frame', 'match');
     if (size(actual_line_token) == 1 & poseFound == 0)
         [xPosition, yPosition, yawRotation] = get_model_pose(actual_line);
         poseFound = 1;
+        fprintf("Yaw returned is: [" + yawRotation + "]\n")
     end
     
+    % Look for the model scale
     actual_line_token = regexp(actual_line, '<scale>', 'match');
     if (size(actual_line_token) == 1 & scaleFound == 0)
         [xScale, yScale] = get_model_scale(actual_line);
@@ -89,24 +96,29 @@ while(actual_line ~= '-1')
         actual_box      = regexp(model_name, 'box', 'match');
         actual_cylinder = regexp(model_name, 'cylinder', 'match');
 
-        % if it is a box
+        % if the model is a box
         if (size(actual_box) == 1)
             b = zeros(1, 8);
             [b(1,1), b(1,2), b(1,3), ... 
-                b(1,4), b(1,5), b(1,6), ...
-                b(1,7), b(1,8)] ...
-                = box_corners(xPosition, yPosition, xScale, yScale, yawRotation*180/pi, model_name);
+             b(1,4), b(1,5), b(1,6), ...
+             b(1,7), b(1,8)] ...
+             = box_corners(xPosition, yPosition, xScale, yScale, yawRotation, model_name);
             figuresMatrix = [figuresMatrix; b(1:8)];
         end
+        
+        % if the model is a cylinder
         if(size(actual_cylinder) == 1)
             c = zeros(1, 3);
             [c(1,1), c(1,2), c(1,3)] = cylinder_dimensions(xPosition, yPosition, xScale, model_name);
-            cilindersMatrix = [cilindersMatrix; c(1:3)];
+            cylindersMatrix = [cylindersMatrix; c(1:3)];
         end
     end
 
+    % Pick a new line to analyze
     actual_line = string(fgetl(base_file_id));    
 end
+
+% Construct the graph
 writematrix(figuresMatrix, 'figures_container.xlsx')
 data = xlsread('figures_container.xlsx');
 data = reshape(data, [], 2, 4);
@@ -135,6 +147,7 @@ function [xPos, yPos, yawRot] = get_model_pose(actual_line)
       xPos      = str2double(out(1));
       yPos      = str2double(out(2));
       yawRot    = str2double(out(6));
+      fprintf("Real yaw is: [" + yawRot + "]\n")
 end
 
 function [xScale, yScale] = get_model_scale(actual_line)
@@ -146,16 +159,17 @@ end
 
 %%
 % Function to calculate figures dimensions to plot
-function [b2, b3, b4, b5, b6, b7, b8, b9] = box_corners(Center_X, Center_Y, Scale_X, Scale_Y, Rotation_deg, boxName)
+function [b2, b3, b4, b5, b6, b7, b8, b9] = box_corners(Center_X, Center_Y, Scale_X, Scale_Y, Rotation_rad, boxName)
     b1 = boxName;
-    b6 = Center_X + Scale_X/2*cos(Rotation_deg) + Scale_Y/2*sin(Rotation_deg);
-    b7 = Center_Y + Scale_X/2*sin(Rotation_deg) - Scale_Y/2*cos(Rotation_deg);
-    b8 = Center_X + Scale_X/2*cos(Rotation_deg) - Scale_Y/2*sin(Rotation_deg);
-    b9 = Center_Y + Scale_X/2*sin(Rotation_deg) + Scale_Y/2*cos(Rotation_deg);   
-    b4 = Center_X - Scale_X/2*cos(Rotation_deg) + Scale_Y/2*sin(Rotation_deg);
-    b5 = Center_Y - Scale_X/2*sin(Rotation_deg) - Scale_Y/2*cos(Rotation_deg);
-    b2 = Center_X - Scale_X/2*cos(Rotation_deg) - Scale_Y/2*sin(Rotation_deg);
-    b3 = Center_Y - Scale_X/2*sin(Rotation_deg) + Scale_Y/2*cos(Rotation_deg);
+    b2 = Center_X - Scale_X/2*cos(Rotation_rad) - Scale_Y/2*sin(Rotation_rad);
+    b3 = Center_Y - Scale_X/2*sin(Rotation_rad) + Scale_Y/2*cos(Rotation_rad);
+    b4 = Center_X - Scale_X/2*cos(Rotation_rad) + Scale_Y/2*sin(Rotation_rad);
+    b5 = Center_Y - Scale_X/2*sin(Rotation_rad) - Scale_Y/2*cos(Rotation_rad);
+    b6 = Center_X + Scale_X/2*cos(Rotation_rad) + Scale_Y/2*sin(Rotation_rad);
+    b7 = Center_Y + Scale_X/2*sin(Rotation_rad) - Scale_Y/2*cos(Rotation_rad);
+    b8 = Center_X + Scale_X/2*cos(Rotation_rad) - Scale_Y/2*sin(Rotation_rad);
+    b9 = Center_Y + Scale_X/2*sin(Rotation_rad) + Scale_Y/2*cos(Rotation_rad);   
+
 end
 
 function [c2, c3, c4] = cylinder_dimensions(Center_X, Center_Y, Radius, cylinderName)
